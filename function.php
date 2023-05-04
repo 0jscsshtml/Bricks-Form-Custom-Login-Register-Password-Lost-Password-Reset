2747da448f41dd258dee1da319dccb038400fefc
0jscsshtml <80338568+0jscsshtml@users.noreply.github.com>
2023-05-04 21:02:23 +0800

    Update function.php

--- function.php
+++ function.php
@@ -87,12 +87,16 @@ function redirect_logged_in_user( $redirect_to = null ) {
     	}
 }
 
-/*** Redirect default after logout to home page ***/ 
-add_action( 'wp_logout', function() {
-	$redirect_url = home_url();
-    	wp_safe_redirect( $redirect_url );
-    	exit;
-});
+/*** logout without confirmation and redirect***/
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result) {
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : 'https://domain.com'; // change redirect url here
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
 
 /*** Registration form password match, email domain blacklist validation ***/
 add_filter( 'bricks/form/validate', function( $errors, $form ) {
